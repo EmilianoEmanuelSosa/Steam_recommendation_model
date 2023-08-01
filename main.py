546486822f644d0f03bd06d56e965af8059e4e28
EmilianoEmanuelSosa <emilianoemauel2850@gmail.com>
2023-08-01 15:46:41 -0300

    <add>Datasets

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from fastapi import FastAPI
+
+
+def genero( Año: str ):
+    print('ok..')
+
+def juegos( Año: str ):
+    print('ok..')
+
+def specs( Año: str ):
+    print('ok..')
+
+def earlyacces( Año: str ):
+    print('ok..')
+
+def sentiment( Año: str ):
+    print('ok..')
+
+def metascore( Año: str ):
+    print('ok..')
