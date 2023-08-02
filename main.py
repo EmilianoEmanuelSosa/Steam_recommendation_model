6c6e12db1563548ed2225e3e3777075c266e9914
EmilianoEmanuelSosa <emilianoemauel2850@gmail.com>
2023-08-02 19:04:14 -0300

    <add>Datasets

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import pandas as pd
 import numpy as np
 from fastapi import FastAPI
 
+app = FastAPI()
 
-def genero( Año: str ):
+@app.get('/genre_most_year/{idioma}')
+def genero( Ano: str ):
+    return 'ok'
+
+@app.get('/list_Games_per_year/{idioma}')
+def juegos( year: str ):
     print('ok..')
 
-def juegos( Año: str ):
-    print('ok..')
-
+@app.get('/Specs/{idioma}')
 def specs( Año: str ):
     print('ok..')
 
+@app.get('/Games_early_acces/{idioma}')
 def earlyacces( Año: str ):
     print('ok..')
 
+@app.get('/sentiment_year/{idioma}')
 def sentiment( Año: str ):
     print('ok..')
 
+@app.get('/game_meta_score/{idioma}')
 def metascore( Año: str ):
     print('ok..')
