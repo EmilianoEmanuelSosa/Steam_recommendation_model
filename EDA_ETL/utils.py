4bfda3dc8387fc913a2342849cee48e188018c4a
EmilianoEmanuelSosa <emilianoemauel2850@gmail.com>
2023-08-03 23:02:34 -0300

    <add>Datasets

---
+++ EDA_ETL/utils.py
@@ -0,0 +1,8 @@
+# Calculate the mode of the 'genres' column
+def get_mode_list(lst):
+    # Use the Counter class from collections to count the occurrences of each list in the 'genres' column
+    from collections import Counter
+    counter = Counter(lst)
+    # Find the most common list (the mode) and return it
+    mode_list = counter.most_common(1)[0][0]
+    return mode_list
