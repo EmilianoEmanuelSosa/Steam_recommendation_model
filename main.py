b0a7e8f3581995c412741576a97be71e812494c0
EmilianoEmanuelSosa <emilianoemauel2850@gmail.com>
2023-08-06 12:29:44 -0300

    <add>Datasets

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from fastapi import FastAPI
 import re
+from collections import Counter
 
 app = FastAPI()
 
@@ -51,6 +52,77 @@ def genero(Ano: str):
      return genre_count_dict
 
 
+@app.get('/juegos/{year}')
+def juegos(year: str):
+    """
+    Returns a list of games released in a specific year.
+
+    Args:
+        year (str): Year for which you want to retrieve the list of games.
+
+    Returns:
+        list: List of games released in the specified year.
+    """
+    # Check if the year contains only digits and is within the accepted range (e.g., 1983 to 2019)
+    if not re.match(r'^\d{4}$', year):
+        return {"error": "Year must be a 4-digit number"}
+
+    # Convert the year to an integer for filtering
+    year = int(year)
+
+    # Check if the year is within the accepted range (1983 to 2019)
+    if year < 1983 or year > 2019:
+        return {"error": "The year must be between 1983 and 2019"}
+
+    # Filter the data by the selected year
+    games_by_year = [(row['app_name'], row['year']) for index, row in df_juegos.iterrows() if row['year'] == year]
+
+    # Extract the game names from the filtered data
+    game_names = [game[0] for game in games_by_year]
+
+    return game_names
+
+
+
+
+from collections import Counter
+import ast
+
+@app.get('/Specs/{Ano}')
+def specs(Ano: str):
+    """
+    Returns a dictionary with the 5 most repeated specs for a specific year.
+
+    Args:
+        Ano (str): Year for which you want to count the specs.
+
+    Returns:
+        dict: Dictionary with the year as key and a list of strings containing the spec.
+    """
+    # Check if the year contains only digits and is within the accepted range (e.g., 1983 to 2019)
+    if not re.match(r'^\d{4}$', Ano):
+        return {"error": "Year must be a 4-digit number"}
+
+    # Convert the year to an integer for filtering
+    year = int(Ano)
+
+    # Check if the year is within the accepted range (1983 to 2019)
+    if year < 1983 or year > 2019:
+        return {"error": "The year must be between 1983 and 2019"}
+
+    # Filter the data by the selected year
+    specs_by_year = [row['specs'] for index, row in df_specs.iterrows() if row['year'] == year]
+
+    # Convert lists to strings and count their occurrences
+    specs_count = Counter([str(spec) for spec in specs_by_year])
+
+    # Get the top 5 specs as a list of strings containing the spec
+    top_specs_list = [spec.strip('[]') for spec, _ in specs_count.most_common(5)]
+
+    # Create the final dictionary structure
+    result_dict = {str(year): top_specs_list}
+
+    return result_dict
 
 
 
@@ -58,22 +130,123 @@ def genero(Ano: str):
 
 
 
-@app.get('/list_Games_per_year/{idioma}')
-def juegos( year: str ):
-    print('ok..')
 
-@app.get('/Specs/{idioma}')
-def specs( Año: str ):
-    print('ok..')
 
-@app.get('/Games_early_acces/{idioma}')
-def earlyacces( Año: str ):
-    print('ok..')
+@app.get('/earlyacces/{Ano}')
+def earlyacces(Ano: str):
+    """
+    Returns the number of games released with early access in a specific year.
 
-@app.get('/sentiment_year/{idioma}')
-def sentiment( Año: str ):
-    print('ok..')
+    Args:
+        Ano (str): Year for which you want to count the games with early access.
 
-@app.get('/game_meta_score/{idioma}')
-def metascore( Año: str ):
-    print('ok..')
+    Returns:
+        dict: Dictionary with the year as key and the number of games with early access as value.
+    """
+    # Check if the year contains only digits and is within the accepted range (e.g., 1983 to 2019)
+    if not re.match(r'^\d{4}$', Ano):
+        return {"error": "Year must be a 4-digit number"}
+
+    # Convert the year to an integer for filtering
+    year = int(Ano)
+
+    # Check if the year is within the accepted range (1983 to 2019)
+    if year < 1983 or year > 2019:
+        return {"error": "The year must be between 1983 and 2019"}
+
+    # Filter the data by the selected year and early access flag
+    games_with_early_access = df_earlyacces[(df_earlyacces['year'] == year) & (df_earlyacces['early_access'] == True)]
+
+    # Get the count of games with early access
+    count_early_access = len(games_with_early_access)
+
+    # Create the final dictionary structure
+    result_dict = {str(year): count_early_access}
+
+    return result_dict
+
+
+
+
+@app.get('/sentiment_year/{Ano}')
+def sentiment(Ano: str):
+    """
+    Returns a dictionary with the count of sentiment categories for a specific year.
+
+    Args:
+        Ano (str): Year for which you want to count the sentiment categories.
+
+    Returns:
+        dict: Dictionary with the sentiment category as key and the count as value, ordered by count in descending order,
+              excluding the categories with "user reviews" in the key.
+    """
+    # Check if the year contains only digits and is within the accepted range (e.g., 1983 to 2019)
+    if not re.match(r'^\d{4}$', Ano):
+        return {"error": "Year must be a 4-digit number"}
+
+    # Convert the year to an integer for filtering
+    year = int(Ano)
+
+    # Check if the year is within the accepted range (1983 to 2019)
+    if year < 1983 or year > 2019:
+        return {"error": "The year must be between 1983 and 2019"}
+
+    # Filter the data by the selected year and sentiment column
+    sentiments_by_year = [row['sentiment'] for index, row in df_sentiment.iterrows() if row['year'] == year]
+
+    # Count the occurrences of each sentiment category
+    sentiment_count = Counter(sentiments_by_year)
+
+    # Remove categories with "user reviews" in the key
+    filtered_sentiment = {k: v for k, v in sentiment_count.items() if "user reviews" not in k}
+
+    # Sort the sentiment categories by count in descending order
+    sorted_sentiment = dict(sorted(filtered_sentiment.items(), key=lambda x: x[1], reverse=True))
+
+    return sorted_sentiment
+
+
+
+
+
+@app.get('/game_meta_score/{Ano}')
+def metascore(Año: str):
+    """
+    Returns a dictionary with the top 5 games for a specific year with the highest metascore.
+
+    Args:
+        Año (str): Year for which you want to find the top 5 games.
+
+    Returns:
+        dict: Dictionary with the game name as key and its metascore as value, for the top 5 games of the specified year.
+    """
+    # Check if the year contains only digits and is within the accepted range (e.g., 1983 to 2019)
+    if not re.match(r'^\d{4}$', Año):
+        return {"error": "Year must be a 4-digit number"}
+
+    # Convert the year to an integer for filtering
+    year = int(Año)
+
+    # Check if the year is within the accepted range (1983 to 2019)
+    if year < 1983 or year > 2019:
+        return {"error": "The year must be between 1983 and 2019"}
+
+    # Filter the data by the selected year
+    games_by_year = df_metascore[df_metascore['year'] == year]
+
+    # Drop rows with missing values in metascore
+    games_by_year = games_by_year.dropna(subset=['metascore'])
+
+    # Convert the metascore column to numeric values
+    games_by_year['metascore'] = pd.to_numeric(games_by_year['metascore'], errors='coerce')
+
+    # Sort the games by metascore in descending order
+    sorted_games = games_by_year.sort_values(by='metascore', ascending=False)
+
+    # Take the top 5 games with the highest metascore
+    top_5_games = sorted_games.head(5)
+
+    # Create a dictionary with game name as key and metascore as value for each of the top 5 games
+    top_5_dict = {row['app_name']: row['metascore'] for _, row in top_5_games.iterrows()}
+
+    return top_5_dict
