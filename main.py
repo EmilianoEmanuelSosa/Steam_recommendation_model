492ec061326c63beebeb3572ef4d5429f8ab8c81
EmilianoEmanuelSosa <emilianoemauel2850@gmail.com>
2023-08-06 10:04:06 -0300

    <add>Datasets

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from fastapi import FastAPI
+import re
 
 app = FastAPI()
 
@@ -16,17 +17,46 @@ df_metascore=pd.read_csv('Datasets/Data_Processed/def_metascore.csv')
 
 #Endpoints of API
 
-@app.get('/genre_most_year/{idioma}')
-def genero( Ano: str ):
-    reg_year_selected=df_genero[df_genero['year']==int(Ano)]
-    # Count the values in the desired column
-    genre_counts = df_genero['genre'].explode().value_counts()
-    # Count the values in the desired column
-    genre_counts = df_genero['genres'].explode().value_counts()
+@app.get('/genero/{Ano}')
+def genero(Ano: str):
+     """
+     Returns a dictionary showing the number of games by genre for a specific year.
 
-    # Create the dictionary from the string
-    gender_count_dict = genre_counts.to_dict()
-    return gender_count_dict
+     args:
+         year (str): Year for which you want to count the video game genres.
+
+     Returns:
+         dict: Dictionary with the genres as keys and the number of video games in each genre as values.
+     """
+
+     # Check if the year contains only digits and is within the accepted range (1983 to 2019)
+     if not re.match(r'^\d{4}$', Ano):
+         return {"error": "Year must be a 4-digit number"}
+
+     year = int(Ano)
+     if year < 1983 or year > 2019:
+         return {"error": "The year must be between 1983 and 2019"}
+
+     # Filter the data by the selected year
+     reg_year_selected = df_genero[df_genero['year'] == year]
+
+     # Split genres that are separated by commas and remove brackets and quotes
+     genres = reg_year_selected['genres'].str.split(', ')
+     genre_list = [genre.strip("[]'") for sublist in genres for genre in sublist]
+
+     # Count the number of video games by genre and create a dictionary
+     genre_count = pd.Series(genre_list).value_counts()
+     genre_count_dict = {genre.strip("'"): count for genre, count in genre_count.items()}
+
+     return genre_count_dict
+
+
+
+
+
+
+
+
 
 @app.get('/list_Games_per_year/{idioma}')
 def juegos( year: str ):
